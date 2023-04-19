9250246925fd11aed3457f17dd7e574811156e5d
AlinaBerns <123542234+AlinaBerns@users.noreply.github.com>
2023-04-19 13:27:14 +0200

    hoek normalisatie?
    een hoek buiten het bereik naar een hoek binnen het opgegeven bereik brengen. Tegelijkertijd moet de hoek "visueel" hetzelfde blijven.

--- src/be/intecbrussel/robot/App.java
+++ src/be/intecbrussel/robot/App.java
@@ -6,6 +6,7 @@ public class App {
         BendingRobot bendingRobot = new BendingRobot("Rick", 5);
         bendingRobot.bend(6);
         bendingRobot.bend(2);
+        System.out.println(bendingRobot.normalize360(-34));
 
         LiftingRobot liftingRobot = new LiftingRobot("Ann", 100);
         liftingRobot.lift(101);
--- src/be/intecbrussel/robot/BendingRobot.java
+++ src/be/intecbrussel/robot/BendingRobot.java
@@ -8,13 +8,28 @@ public class BendingRobot extends Robot {
 
     //methods
 
-    public void bend (double bendAngle) { //????????
+    public void bend (double bendAngle) { //hoek normalisatie????????
 
         if(bendAngle > this.maxBendAngle || bendAngle < 0) {
-            System.out.println("Bending is not allowed");
+                System.out.println("Bending is not allowed");
         } else {
             System.out.println("The bending was successful. Required bending angle: " + bendAngle + "°");
         }
+    }
+
+    public double normalize360 (double bendAngle) {
+        //hoek normalisatie????????
+        //een hoek buiten het bereik naar een hoek binnen
+        //het opgegeven bereik brengen.
+        // Tegelijkertijd moet de hoek "visueel" hetzelfde blijven.
+
+        bendAngle = bendAngle % 360;
+
+        if (bendAngle < 0) {
+            bendAngle = bendAngle + 360;
+        }
+
+        return bendAngle;
     }
 
     //constructors
